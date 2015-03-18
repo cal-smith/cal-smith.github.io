3c79705a56c55f1d12736ba05e66a905d2067198
Callum Smith <callum1234moo@gmail.com>
2015-03-18 14:28:45 -0400

    tweaked the circle code

--- _site/circles.js
+++ _site/circles.js
@@ -1,16 +1,18 @@
+"use strict";
+
 function e (e){
 	return document.getElementById(e);
 }
 var canvas = e('c');
-client = {x:window.innerWidth-16, y:window.innerHeight-16}
+var client = {x:window.innerWidth-16, y:window.innerHeight-16};
 canvas.width = client.x;
 canvas.height = client.y;
 var numcircles = 200*(client.x/client.y);
 var ctx = canvas.getContext('2d');
 var right = true;
 var bottom = false;
-v = new Date("Feb 14");
-d = new Date();
+var v = new Date("Feb 14");
+var d = new Date();
 var valentine = false;
 if (d.getMonth() === v.getMonth() && d.getDate() === v.getDate()) {
 	valentine = true;
@@ -22,31 +24,18 @@ window.addEventListener('resize', function(e){
 	canvas.height = client.y;
 });
 
-window.addEventListener('mousemove', function(e){	
-	if (e.x > client.x - client.x/8){
-		right = false;
-	} else if (e.x < client.x/8) {
-		right = true;
-	}
-	if (e.y > client.y - client.y/8){
-		bottom = true;
-	} else if (e.y < client.y/8) {
-		bottom = false;
-	}
-});
-
 var Circle = function (point, s, ctx, color){
 	this.x = point.x;
 	this.y = point.y;
 	this.point = point;
-	this.size = 50*s;
 	this.scale = s;
+	this.size = 50*this.scale;
 	this.ctx = ctx;
 	this.color = (typeof color === 'undefined')?"blue":color;
 };
 
 Circle.prototype.render = function() {
-	this.x += right?0.1:-0.1
+	this.x += right?0.1:-0.1;
 	this.y += bottom?0.1:-0.1;
 	this.ctx.fillStyle = this.color;
 	this.ctx.beginPath();
@@ -58,8 +47,8 @@ var Heart = function (point, s, ctx, color){
 	this.x = point.x;
 	this.y = point.y;
 	this.point = point;
-	this.size = 50*s;
 	this.scale = s;
+	this.size = 50*this.scale;
 	this.ctx = ctx;
 	this.color = (typeof color === 'undefined')?"blue":color;
 };
@@ -77,18 +66,30 @@ Heart.prototype.render = function() {
 	this.ctx.fill();
 };
 
-circles = [];
+var circles = [];
 function generate(){
+	var point = {x:Math.random()*(client.x), y:Math.random()*(client.y)};
 	return !valentine?
-	new Circle({x:(Math.random()*(client.x*2)|0)-client.x, 
-		y:(Math.random()*(client.y*2)|0)-client.y}, 
-		Math.random()*(5-0.5+1)+0.5, ctx, 
-		"rgba("+(Math.random()*255|0)+","+(Math.random()*255|0)+","+(Math.random()*255|0)+","+(Math.random()*1)+")"):
+	new Circle(point, Math.random()*(5-0.5+1)+0.5, ctx, 
+		"rgba("+(Math.random()*255|0)+","+(Math.random()*255|0)+","+(Math.random()*255|0)+","+(Math.random()*(0.9 - 0.2)+0.2)+")"):
 	new Heart({x:(Math.random()*(client.x*2)|0)-client.x, 
 		y:(Math.random()*(client.y*2)|0)-client.y}, 
 		Math.random()*(5-0.5+1)+0.5, ctx, 
 		"rgba("+(Math.random()*255|0)+","+(Math.random()*255|0)+","+(Math.random()*255|0)+","+(Math.random()*1)+")");
 }
+
+function circle (point, scale, ctx, color) {
+	var size = 50*scale;
+	return function() {
+		point.x += right?0.1:-0.1;
+		point.y += bottom?0.1:-0.1;
+		ctx.fillStyle = color;
+		ctx.beginPath();
+		ctx.arc(point.x*scale, point.y*scale, size, 0, 2*Math.PI);
+		ctx.fill();
+	}
+}
+
 while(circles.length < numcircles){
 	circles.push(generate());
 }
@@ -101,18 +102,16 @@ function render(t){
 		bottom = !bottom;
 	}
 	ctx.clearRect(0, 0, client.x, client.y);
-	while(circles.length < numcircles){
-		circles.push(generate());
-	}
 	for (var i = 0; i < circles.length; i++) {
+		if ((circles[i].x > client.x) || (circles[i].x < -circles[i].size)) {
+			circles[i] = generate();
+		}
+
+		if ((circles[i].y > client.y) || (circles[i].y < -circles[i].size)) {
+			circles[i] = generate();
+		}
 		circles[i].render();
 	}
-	circles = circles.filter(function(c){
-		if ((c.x > client.x+c.size) || (c.x < 0-c.size) || (c.y > client.y+c.size) || (c.y < 0-c.size)) {
-			return false;
-		}
-		return true;
-	});
 }
 
 function frame (t){
